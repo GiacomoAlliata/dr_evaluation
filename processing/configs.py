0a5bafb9a68bf0ff580af1843ca850af83e0ec9e
giacomoalliata <giacomo.alliata@univ-rennes2.fr>
2026-02-24 17:26:27 +0100

    clean for publication

---
+++ processing/configs.py
@@ -0,0 +1,22 @@
+# Colors for visualisations
+SINGLE_COLOR = "#1f77b4"
+
+COLORS_4 = [
+    "#0072B2",  # blue
+    "#E69F00",  # orange
+    "#009E73",  # bluish green
+    "#CC79A7"   # reddish purple
+]
+
+COLORS_10 = [
+    "#E69F00",  # orange
+    "#56B4E9",  # sky blue
+    "#009E73",  # bluish green
+    "#F0E442",  # yellow
+    "#0072B2",  # blue
+    "#D55E00",  # vermillion
+    "#CC79A7",  # reddish purple
+    "#999999",  # grey
+    "#000000",  # black
+    "#44AA99"   # teal (extension)
+]
